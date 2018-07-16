01273e931930e150ca25c63dc57e3c19b0936beb
kimdugong <js0628@ymail.com>
2018-07-17 08:00:40 +0900

    170까지 완료

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
   state = {
-    minimumContribution: ''
+    minimumContribution: '',
+    errorMessage: '',
+    loading: false
   };
 
   onSubmit = async event => {
     event.preventDefault();
-    const accounts = await web3.eth.getAccounts();
-    await factory.methods.createCampaign(this.state.minimumContribution).send({
-      from: accounts[0]
-    });
+
+    this.setState({ loading: true, errorMessage: '' });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0]
+        });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
   };
+
   render() {
     return (
       <Layout>
         <h3>Create a Campaign</h3>
 
-        <Form onSubmit={this.onSubmit}>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
@@ -33,7 +47,11 @@ class CampaignNew extends Component {
               }
             />
           </Form.Field>
-          <Button primary>Create</Button>
+
+          <Message error heeader="Ooops!" content={this.state.errorMessage} />
+          <Button primary loading={this.state.loading}>
+            Create
+          </Button>
         </Form>
       </Layout>
     );

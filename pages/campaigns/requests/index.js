032730ea657dfd36306bfe19f9e3843951d7978b
kimdugong <js0628@ymail.com>
2018-08-02 00:33:01 +0900

    200까지 완료

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
+import Layout from '../../../components/Layout';
+import { Button } from 'semantic-ui-react';
+import { Link } from '../../../routes';
+import Campaign from '../../../ethereum/campaign';
 
 class RequestIndex extends Component {
+  static async getInitialProps(props) {
+    const { address } = props.query;
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+
+    const requests = await Promise.all(
+      Array(requestCount)
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+    console.log(requests);
+    return { address, requests };
+  }
+
   render() {
-    return <h3>Request Link</h3>;
+    return (
+      <Layout>
+        <h3>Requests</h3>
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary> Add Requests </Button>
+          </a>
+        </Link>
+      </Layout>
+    );
   }
 }
 
